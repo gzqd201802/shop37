db26652b173709f0458b39339282beb5a30dd294
Sujiehao <sujiehao@itcast.cn>
2019-11-14 15:14:01 +0800

    数据初次渲染

--- pages/goods_list/goods_list.js
+++ pages/goods_list/goods_list.js
@@ -1,3 +1,6 @@
+import {
+  myRequest
+} from '../../utils/request.js'
 // pages/goods_list/goods_list.js
 Page({
 
@@ -5,6 +8,26 @@ Page({
    * 页面的初始数据
    */
   data: {
+    goodsData: {}
+  },
+
+  // 封装列表页请求数据
+  getGoods(obj) {
+
+    myRequest({
+      // url 路径，注意有 goods 
+      url: 'goods/search',
+      // data 请求的参数
+      data: {
+        // cid 从分类页过来的 cid 值
+        // cid: "5"
+        ...obj
+      }
+    }).then(res => {
+      this.setData({
+        goodsData: res
+      })
+    })
 
   },
 
@@ -17,7 +40,12 @@ Page({
       cid,
       query
     } = options;
-    console.log(cid);
+    // console.log(cid);
+
+    this.getGoods({
+      // 传递 cid，根据分类 id 查询数据
+      cid
+    });
   },
 
   /**
