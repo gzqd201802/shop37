b6788e222e78c7f72d0136a4e9ec1d5acc180f63
sujiehao <sujiehao@itcast.cn>
2019-11-16 17:40:17 +0800

    购物车数据渲染

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -5,6 +5,37 @@ Page({
    * 页面的初始数据
    */
   data: {
+    cartArr:[]
+  },
+
+  // 加减号改变数量
+  changeCount(e){
+    const {
+      // 加减数量
+      number,
+      // 当前商品索引值
+      index,
+    } = e.currentTarget.dataset;
+    // console.log(number, index);
+
+    // 获取列表数据
+    const {
+      cartArr
+    } = this.data;
+
+    // 根据索引找到数据，改变 number 数量
+    cartArr[index].goods_count += number;
+    
+    // 判断是否越界，减号不能变负数，加号不能超过库存
+
+
+    // 更新视图
+    this.setData({
+      cartArr
+    });
+
+    // 更新本地存储
+    wx.setStorageSync('cartArr', cartArr);
 
   },
 
@@ -12,7 +43,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log('onLoad')
   },
 
   /**
@@ -26,6 +57,13 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    console.log('onShow');
+    const cartArr = wx.getStorageSync('cartArr') || [];
+
+    this.setData({
+      cartArr
+    })
+
 
   },
 
@@ -33,14 +71,14 @@ Page({
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    console.log('onHide')
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    console.log('onUnload')
   },
 
   /**
