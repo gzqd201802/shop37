da18863ea3dcaa17fe700b6227ffd1a632dddd0c
sujiehao <sujiehao@itcast.cn>
2019-11-17 09:59:37 +0800

    购物车列表_加减号_选择按钮_价格更新

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -5,11 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-    cartArr:[]
+    cartArr: [],
+    totalMoney: 0,
+    totalCount: 0,
+    checkAll: false
   },
 
   // 加减号改变数量
-  changeCount(e){
+  changeCount(e) {
     const {
       // 加减数量
       number,
@@ -23,82 +26,121 @@ Page({
       cartArr
     } = this.data;
 
-    // 根据索引找到数据，改变 number 数量
-    cartArr[index].goods_count += number;
-    
-    // 判断是否越界，减号不能变负数，加号不能超过库存
 
+    // 判断是否越界，减号不能变负数，加号不能超过库存
+    if (cartArr[index].goods_count === 1 && number === -1) {
+      wx.showModal({
+        content: '是否要删除商品',
+        // 确定文字
+        confirmText: '删除',
+        confirmColor: '#f00',
+        success: res => {
+          // 在点击确认按钮后
+          if (res.confirm) {
+            // 删除商品
+            cartArr.splice(index, 1);
+            // 记得在异步回调函数内部更新数据
+            this.updateCart(cartArr);
+          }
+        }
+      });
+    } else {
+      // 根据索引找到数据，改变 number 数量
+      cartArr[index].goods_count += number;
+      this.updateCart(cartArr);
+    }
+
+  },
+
+  // 改变选中状态
+  changeCheck(e) {
+    const {
+      // 当前商品索引值
+      index,
+    } = e.currentTarget.dataset;
+
+    // 解构获取购物车数据
+    const {
+      cartArr
+    } = this.data;
+
+    // 获取当前商品的选中状态，取反，再赋值更新
+    cartArr[index].goods_checked = !cartArr[index].goods_checked;
+
+    // 调用封装的方法，更新购物车列表，更新总价格，更新选中件数，更新全选按钮，更新本地存储
+    this.updateCart(cartArr);
+
+
+  },
+  // 全选按钮
+  changeCheckAll() {
+    let {
+      checkAll,
+      cartArr
+    } = this.data;
+
+    cartArr.forEach(v => {
+      v.goods_checked = !checkAll;
+    });
+
+    this.updateCart(cartArr);
+  },
+
+  // 封装的方法，更新购物车列表，更新总价格，更新选中件数，更新全选按钮，更新本地存储
+  updateCart(cartArr) {
+
+    // 总价格
+    let totalMoney = 0;
+    // 商品总数
+    let totalCount = 0;
+
+    // 遍历购物车数组
+    cartArr.forEach(v => {
+      if (v.goods_checked) {
+        totalMoney += v.goods_count * v.goods_price;
+        totalCount++;
+      }
+    });
+
+    // 全选状态：购物车总商品长度 === 选择状态长度
+    // console.log(cartArr.length === totalCount)
 
     // 更新视图
     this.setData({
-      cartArr
+      // 更新总价格
+      totalMoney,
+      // 更新选中商品数
+      totalCount,
+      // 更新购物车数据
+      cartArr,
+      // 更新全选状态
+      checkAll: cartArr.length === totalCount
     });
 
     // 更新本地存储
     wx.setStorageSync('cartArr', cartArr);
-
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     console.log('onLoad')
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
     console.log('onShow');
     const cartArr = wx.getStorageSync('cartArr') || [];
 
-    this.setData({
-      cartArr
-    })
+    this.updateCart(cartArr);
+    // this.setData({
+    //   cartArr
+    // })
 
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    console.log('onHide')
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    console.log('onUnload')
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
